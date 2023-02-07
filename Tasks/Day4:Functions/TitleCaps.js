c0409965322ee5af5f5b4a1af706cee6d2cbeca4
VARSHA <123050756+Varsha11022002@users.noreply.github.com>
2023-02-07 22:45:12 +0530

    Create TitleCaps.js

---
+++ Tasks/Day4:Functions/TitleCaps.js
@@ -0,0 +1,28 @@
+ANONYMOUS FNCT
+  var title= function(msg){
+      
+      var input=msg;
+     
+      for(let i=0;i<=input.length;i++)
+      {
+         
+          console.log(input[i][0].toUpperCase()+input[i].substr(1).toLowerCase())
+      }
+      
+      }
+
+title(["Batman","superman","sPIDERman"])
+
+
+IIFE
+ (function(msg){
+      
+      var input=msg;
+     
+      for(let i=0;i<=input.length;i++)
+      {
+         
+          console.log(input[i][0].toUpperCase()+input[i].substr(1).toLowerCase())
+      }
+      
+      })(["Batman","superman","sPIDERman"])
