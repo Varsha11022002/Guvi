eca0759d8cd29bbdd1f15e0b8fbe3ecdf967e5c0
VARSHA <123050756+Varsha11022002@users.noreply.github.com>
2023-02-07 23:00:06 +0530

    Update ChkPrimeorNot.js

--- Tasks/Day4:Functions/ChkPrimeorNot.js
+++ Tasks/Day4:Functions/ChkPrimeorNot.js
@@ -43,4 +43,27 @@ _____________________________________________________________
          
       })    
         
-      })  ([2,13,5,17,4,6,8])  
+      })  ([2,13,5,17,4,6,8]) 
+
+
+___________________________________________________
+ARROW FNCT
+ var prime=(msg)=> {      
+      msg.forEach((ele)=>{          
+         if(checkprime(ele)){
+             console.log(ele)
+         }})       
+      }
+      
+      function checkprime(n){
+    for(let j=2;j<n;j++){
+             if(n%j==0)           
+                return false;            
+          }
+          
+          return true;         
+      }
+      
+      
+   prime([2,3,5,4,6,8]) 
+
