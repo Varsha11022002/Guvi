20ad48d7e1ebc9f40ada90ac74e6ddde9981c6a3
VARSHA <123050756+Varsha11022002@users.noreply.github.com>
2023-02-08 22:03:17 +0530

    Update FindPalindromeinaArray.js

--- Tasks/Day4:Functions/FindPalindromeinaArray.js
+++ Tasks/Day4:Functions/FindPalindromeinaArray.js
@@ -33,6 +33,7 @@ var rev;
     rev=rev.join("");    
    }
    if(word===rev)
+	   
     	console.log(word);
   
 }
@@ -68,5 +69,24 @@ input.forEach((ele)=>
 	palindrome(ele)
 })
 
+_______________________________________________________
+
+IIFE
+((word)=>{
+
+var arr=[]
+var rev;
+for(let k=0;k<word.length;k++){
+	for(let i=0;i<word[k].length;i++)
+  {
+  	
+  	let a=word[k].split("");
+    rev=a.reverse();
+    rev=rev.join("");    
+   }
+   if(word[k]===rev)
+    	console.log(word[k]);
+  }
+})(["malayalam","mom","spiderman","batman","madam"])
  
  
