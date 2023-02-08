ff9a348c865a062783d340c0460470c592e408b9
VARSHA <123050756+Varsha11022002@users.noreply.github.com>
2023-02-08 08:46:02 +0530

    Create DisplayFlags.js

---
+++ XMLHttpRequst/DisplayFlags.js
@@ -0,0 +1,10 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send()
+
+request.onload=function(){
+var data=JSON.parse(this.responseText);
+data.forEach((ele)=>{
+console.log(ele.name.common,ele.flags.png)
+})
+}
